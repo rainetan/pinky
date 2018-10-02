1a0a532af28773d1702b6a00193ecdda200e692b
Phillis Yuan <tigglywuff@gmail.com>
2018-10-02 09:52:14 -0700

    added baby-shark support

--- main.js
+++ main.js
@@ -8,18 +8,40 @@ let db = new sqlite3.Database('./data.db', (err) => {
     if (err) {
         console.log(err.message);
     }
-
     console.log('connected to db');
 })
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+
+function babyShark(msg) {
+    if (!msg.content.toLowerCase().match(/baby.*shark/)) {
+        return;
+    }
+
+    var voiceChannel = msg.member.voiceChannel;
+    console.log(voiceChannel);
+    voiceChannel.join().then(connection => {
+        const dispatcher = connection.playFile('./baby-shark.mp3');
+        dispatcher.setVolume(0.05);
+        dispatcher.on("end", end => {
+            voiceChannel.leave();
+        });
+    }).catch(err => {
+        console.error(err);
+        voiceChannel.leave();
+    });
+}
  
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+
+    babyShark(msg);
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+
+    }
 });
  
 client.login(config.token);
