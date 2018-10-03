e0b94d004eb31f18e9e5db86a4babedfe32836db
Phillis Yuan <tigglywuff@gmail.com>
2018-10-03 02:55:09 -0700

    added baby shark bombing

--- main.js
+++ main.js
@@ -2,46 +2,57 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
-const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./data.db', (err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    console.log('connected to db');
-})
- 
+// let db = new sqlite3.Database('./data.db', (err) => {
+//     if (err) {
+//         console.log(err.message);
+//     }
+//     console.log('connected to db');
+// })
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/* Returns undefined if none, otherwised a Member */
+function getFirstMentionedUser(msg) {
+    return msg.mentions.members.random();
+}
 
-function babyShark(msg) {
+function babyShark(msg) {    
     if (!msg.content.toLowerCase().match(/baby.*shark/)) {
         return;
     }
 
-    var voiceChannel = msg.member.voiceChannel;
-    console.log(voiceChannel);
+    const member = getFirstMentionedUser(msg) || msg.member;
+    const voiceChannel = member.voiceChannel;
+
+    if (!voiceChannel) {
+        return;
+    }
+
+    msg.react('🦈');
+
     voiceChannel.join().then(connection => {
         const dispatcher = connection.playFile('./baby-shark.mp3');
         dispatcher.setVolume(0.05);
+
         dispatcher.on("end", end => {
             voiceChannel.leave();
         });
     }).catch(err => {
-        console.error(err);
+        console.log(err);
         voiceChannel.leave();
     });
 }
  
 client.on('message', msg => {
-
-    babyShark(msg);
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-
+    try {
+        babyShark(msg);
+    } catch (e) {
+        console.log(e);
     }
 });
- 
+
 client.login(config.token);
